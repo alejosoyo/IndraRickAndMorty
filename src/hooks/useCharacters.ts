3708c0ab343f1683ee1187a5af68a4f8090d27f2
Jhon Alejandro Perez <jlejopg@gmail.com>
2024-08-29 10:31:22 -0500

    Use useReducer instead of useState

--- src/hooks/useCharacters.ts
+++ src/hooks/useCharacters.ts
@@ -1,28 +1,59 @@
-import { useState, useEffect } from 'react';
+import { useReducer, useEffect } from 'react';
 import axios from 'axios';
 import config from '../../config';
-import { Character } from '../interfaces/character';
+import { Action, State } from '../interfaces/character';
+
+const initialState: State = {
+  data: [],
+  loading: true,
+  error: null,
+};
+
+const dataFetchReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+      };
+    case 'FETCH_ERROR':
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      throw new Error();
+  }
+};
 
 const useCharacters = () => {
-  const [data, setData] = useState<Character[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const [state, dispatch] = useReducer(dataFetchReducer, initialState);
+
   useEffect(() => {
     const getCharacters = async () => {
+      dispatch({ type: 'FETCH_INIT' });
+
       try {
+        //had a problem with Vite's import.meta for the Tests, for the moment I had to use this method of Config.
         const response = await axios.get(config.API_URL);
-        setData(response.data.results);
+        dispatch({ type: 'FETCH_SUCCESS', payload: response.data.results });
       } catch (err) {
-        setError('Error fetching data');
-      } finally {
-        setLoading(false);
+        dispatch({ type: 'FETCH_ERROR', payload: 'Error fetching data' });
       }
     };
 
     getCharacters();
   }, []);
 
-  return { data, loading, error };
+  return { ...state };
 };
 
 export default useCharacters;
--- src/interfaces/character.ts
+++ src/interfaces/character.ts
@@ -1,6 +1,17 @@
 export interface Character {
-    id: number;
-    name: string;
-    image: string;
-    species: string;
-  }
+  id: number;
+  name: string;
+  image: string;
+  species: string;
+}
+
+export type Action =
+  | { type: 'FETCH_SUCCESS'; payload: Character[] }
+  | { type: 'FETCH_ERROR'; payload: string }
+  | { type: 'FETCH_INIT' };
+
+export interface State {
+  data: Character[];
+  loading: boolean;
+  error: string | null;
+}
